24e7f2b766ff83d4f16e7d2fed188c241294affa
thehamdiaz <ga_hamdi@esi.dz>
2023-10-03 13:10:22 +0200

    adding add() and equals() functions

--- Fraction.java
+++ Fraction.java
@@ -1,13 +1,59 @@
 public class Fraction {
-    // Fields (attributes)
+    // Constant fractions
+    public static final Fraction ZERO = new Fraction(1, 0);
+    public static final Fraction UN = new Fraction(1, 1);
+
+    // attributes
     int num;
     int deno;
     double fraction;
-    // Constructors
+
+    //Constructors
     public Fraction(int num, int deno) {
         this.num = num;
         this.deno = deno;
-        this.fraction = num/deno;
+        this.fraction = (double) num/deno;
+    }
+    public Fraction(int num) {
+        this.num = num;
+        this.deno = 1;
+        this.fraction = (double) num/deno;
+    }
+    public Fraction() {
+        this.num = 0;
+        this.deno = 1;
+        this.fraction = (double) num/deno;
+    }
+    public Fraction(double fraction) {
+        this.fraction = (double) fraction;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public int getDeno() {
+        return deno;
+    }
+
+    public double doubleValue() {
+        return fraction;
+    }
+
+    public Fraction add(Fraction f){
+        //common denominator
+        int commonDeno = this.deno * f.getDeno();
+
+        // Calculate the new numerators
+        int newNum1 = this.num * commonDeno;
+        int newNum2 = f.getNum() * commonDeno;
+
+        int sumNum = newNum1 + newNum2;
+        return new Fraction(sumNum, commonDeno);
+    }   
+
+    public boolean Equals(Fraction f) {
+        return num * deno == f.getNum() * f.getDeno();
     }
 
     public String toString(){
--- Main.java
+++ Main.java
@@ -1,6 +1,24 @@
 public class Main {
   public static void main(String[] args) {
-    Fraction fraction1 = new Fraction(10, 3);
-    System.out.println(fraction1.toString());
-  }
+    Fraction fraction1 = new Fraction(45, 2);
+    Fraction fraction2 = new Fraction(45);
+    Fraction fraction3 = new Fraction();
+
+
+    // Assertion 1
+    assert  fraction1.toString().equals("I am a fraction and my value is: 22.5") : "Desired output: \"I am a fraction and my value is: 22.5\"";
+    // Assertion 2
+    assert  fraction2.toString().equals("I am a fraction and my value is: 45.0") : "Desired output: \"I am a fraction and my value is: 45.0\"";
+    // Assertion 3
+    assert  fraction3.toString().equals("I am a fraction and my value is: 0.0") : "Desired output: \"I am a fraction and my value is: 0.0\"";
+
+    // Assertion 4 
+    assert fraction1.doubleValue() == 22.5 : "This function should return a value of 22.5";
+
+    // assert fraction1.getClass().getName().equals("double") : "this funtion should return a number of type double";
+    System.out.println(fraction1.add(fraction2).toString());
+
+    assert fraction1.add(fraction2).toString().equals("I am a fraction and my value is: 67.5") : "This should return a value of 22.5 + 45 = 67.5";
+}
+
 }
